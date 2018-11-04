c2f82808ddcddf223060b2dca5d91a11f970c916
Elton <eltonp3103@gmail.com>
2018-11-04 01:10:29 -0400

    Made changes to the regex expressions in cal.py

--- cal.py
+++ cal.py
@@ -1,17 +1,17 @@
 import re
 
 def extract_calendar_events(stri):
+
     list_of_patterns = [
-    r'\d\d(/|-)\d\d(/|-)\d\d',
-    r'\d\d(/|-)\m\m(/|-)\d\d\d\d',
-    r'\d(/|-)\d(/|-)\d\d',
-    r'\d(/|-)\d(/|-)\y\y\y\y',
-    r'\d\d?-(Jan|Feb|Mar|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|May|April|July|August|September|October|November|December)-\d\d\d\d',
-    r'\d\d\d\d(/|-)\d\d(/|-)\d\d',
-    r'(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mon|Tue|Tues|Wed|Thurs|Thu|Fri|Sat|Sun)(\s|\S)+\d\d?(\s|\S)+(Jan|Feb|Mar|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|May|April|July|August|September|October|November|December)(\s|\S)+\d\d\d\d',
-    r'(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mon|Tue|Tues|Wed|Thurs|Thu|Fri|Sat|Sun)(\s|\S)+(Jan|Feb|Mar|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|May|April|July|August|September|October|November|December)(\s|\S)+\d\d?(\s|\S)+\d\d\d\d'
+    r'\d\d?(/|-|. | |.)\d\d?(/|-|. | |.)\d\d\d?\d? ?',
+    r'(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mon|Tue|Tues|Wed|Thurs|Thu|Fri|Sat|Sun)?,? +\d\d?(/|-|. | |.)(Jan|Feb|Mar|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|May|April|July|August|September|October|November|December)?(/|-|. | |.)\d\d\d?\d? ?', # you made a change here in the months, added ?; if an error props up , double check that line
+    r'\d\d\d\d(/|-|. | |.)\d\d(/|-|. | |.)\d\d? ?',
+    r'\d\d\d\d(/|-|. | |.)?(Jan|Feb|Mar|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|May|April|July|August|September|October|November|December)(/|-|. | |.)?\d\d?,? ?(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mon|Tue|Tues|Wed|Thurs|Thu|Fri|Sat|Sun)?',
+    r'(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mon|Tue|Tues|Wed|Thurs|Thu|Fri|Sat|Sun)?,? (Jan|Feb|Mar|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|May|April|July|August|September|October|November|December)?,? \d\d?, \d\d\d?\d? ?',
+    r'\S+ \dth+ \S+ (Jan|Feb|Mar|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January|February|March|May|April|July|August|September|October|November|December) \d\d\d\d ?'
     ]
 
-    for pattern in list_of_patterns:
-        while True:
-            date
+
+s = "Our first general meeting of the semester is coming up soon. It will be next Tuesday, October 23, at 6pm in CoC room 017. Come hear what all of RoboJackets is working on, and see a short presentation by Scott Gilliland about his research. Unfortunately, due to events out of our control, we won't be able to provide pizza for this one. If you can make it, please don't forget to RSVP by clicking this link."
+
+extract_calendar_events(s)
